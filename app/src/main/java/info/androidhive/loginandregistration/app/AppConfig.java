032c2cc83be40115f1096f95e49122d0c03b3972
jccultima123 <johncyrillcorsanes@gmail.com>
2017-03-20 15:05:27 +0800

    Some fixes..

--- app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
+++ app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -2,8 +2,8 @@ package info.androidhive.loginandregistration.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+	public static String URL_LOGIN = "http://192.168.1.20/myphp/index.php";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+	public static String URL_REGISTER = "http://192.168.1.20/myphp/register.php";
 }
